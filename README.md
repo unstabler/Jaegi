6d27afc23af4d62912ac30b7330aa752186aa654
Gyuhwan Park★ <doping.cheese@gmail.com>
2018-11-11 19:35:41 +0900

    initial commit;

---
+++ README.md
@@ -0,0 +1,13 @@
+# Jaegi
+
+Jaegi는 게이 데이팅 앱 [Jack'd](http://jackd.mobi) 에서 구글 플레이 스토어 팝업이 뜨는 것을 막아주는 애플리케이션입니다.
+
+# License / Author
+
+- 작성자: Gyuhwan Park(unstabler) / [twitter: @_unstabler](https://twitter.com/_unstabler)
+
+- **이 애플리케이션은 CC0 1.0 Universal 라이선스 하에 배포됩니다.**
+
+- **이 애플리케이션을 사용함으로써 일어나는 문제에 대해서 이 애플리케이션의 작성자는 일체 책임을 지지 않습니다.**
+
+
---
+++ app/src/main/java/pl/unstabler/jaegi/JDDumbListenerActivity.java
@@ -0,0 +1,44 @@
+package pl.unstabler.jaegi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+public class JDDumbListenerActivity extends Activity {
+    static final String LOG_TAG = "JDDumbListenerActivity";
+    static final String JACKD_PACKAGE = "mobi.jackd.android";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = this.getIntent();
+        if (intent != null) {
+            this.processIntent();
+        }
+
+        this.finish();
+    }
+
+    private void processIntent() {
+        Intent referrerIntent = this.getIntent();
+        String referrer = this.getReferrer().getHost();
+
+
+        Log.d(LOG_TAG, String.format("received intent from %s", referrer));
+
+        if (!JACKD_PACKAGE.equals(referrer)) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setPackage("com.android.vending");
+            intent.setData(referrerIntent.getData());
+            this.startActivity(intent);
+            return;
+        }
+
+        Log.d(LOG_TAG, String.format("uri [%s] has blocked;", referrerIntent.getData().toString()));
+        Toast.makeText(this, R.string.toast_blocked, Toast.LENGTH_SHORT).show();
+    }
+}
